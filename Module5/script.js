5cc5282642370ac645688e972cfd3f68dec64f0c
Mchuong10 <122072731+Mchuong10@users.noreply.github.com>
2023-02-11 17:33:48 -0800

    Update script.js

--- Module5/script.js
+++ Module5/script.js
@@ -1,24 +1,22 @@
+var table = 3;             // Unit of table
+var operator = 'multiplication'; // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = "<h2>Multiplication Table</h2>";    // Message
+
 
 let table;
+
+
 do {
     table = prompt("Provide a value from 0 to 10");
 } while (Number(table) === NaN || table < 0 || table > 10)       
 
-
-
-
-var i = 1;                
-var msg = "<h2>Multiplication Table</h2>";              
-
-
-
+    
     // Do multiplication
     while (i < 11) {
         msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
         i++;
     }
-
-}
 
 
 
