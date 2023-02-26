a0b876c09c404f2ffc77d7c0d7c00e914ff5e405
Mchuong10 <122072731+Mchuong10@users.noreply.github.com>
2023-02-25 21:16:23 -0800

    Update script.js

--- Artifact3/script.js
+++ Artifact3/script.js
@@ -53,3 +53,47 @@ function downRight(pHeight, pColorEven, pColorOdd, pSymbol) {
 
   document.getElementById("downRight").innerHTML = rLine;
 }
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+  var rLine = "";
+  for (i = 0; i < pHeight; i++) {
+    rLine += "<p>";
+    //Create each line on the Rhombus
+    for (j = 0; j <= i; j++) {
+      //Is the position even or odd so we change the color
+      if (j % 2)
+        //even
+        rLine +=
+          "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+      //odd
+      else
+        rLine +=
+          "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+    }
+    rLine += "</p>";
+    // console.log(rLine);
+  }
+  console.log(rLine);
+  document.getElementById("upLeft").innerHTML = rLine;
+}
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+  var rLine = "";
+  for (i = 0; i < pHeight; i++) {
+    rLine += "<p>";
+    //Create each line on the Rhombus
+    for (j = 0; j <= i; j++) {
+      //Is the position even or odd so we change the color
+      if (j % 2)
+        //even
+        rLine +=
+          "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+      //odd
+      else
+        rLine +=
+          "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+    }
+    rLine += "</p>";
+    // console.log(rLine);
+  }
+  console.log(rLine);
+  document.getElementById("downLeft").innerHTML = rLine;
+}
