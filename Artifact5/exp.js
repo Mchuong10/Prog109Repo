6f841a0c805bfd830831d714f87a9cd91855469d
Mchuong10 <122072731+Mchuong10@users.noreply.github.com>
2023-03-04 21:08:18 -0800

    Update exp.js

--- Artifact5/exp.js
+++ Artifact5/exp.js
@@ -28,7 +28,10 @@ function nextimg() {
 function newimg() {
   //Calculate next image subscript number.
   subscript = subscript >= images.length - 1 ? 0 : subscript + 1;
-   
+    //Set next image filename appropriate image name in array.
+  try {
+    var image = images[subscript][0];
+  }
   //Set the image tag's src= and classname= attributes.
   var imgtag = document.getElementById("slideimg");
   imgtag.src = imagefile;
