69b1712cc7cccce1f3afa1c5eb9d69bca0783161
Mchuong10 <122072731+Mchuong10@users.noreply.github.com>
2023-03-07 15:19:49 -0800

    Update exp.js

--- Artifact5/exp.js
+++ Artifact5/exp.js
@@ -2,16 +2,16 @@
  
 //Replace with: relative pathes to your images, alt text, caption
 var myImages = [
-  ["https://github.com/Mchuong10/Prog109Repo/blob/main/Artifact5/20220618_124212.jpg", "Dress-Up"],
-  ["https://github.com/Mchuong10/Prog109Repo/blob/main/Artifact5/20220629_154645.jpg", "Ducks"],
-  ["https://github.com/Mchuong10/Prog109Repo/blob/main/Artifact5/20220813_122309.jpg", "Fun in the Sun"],
-  ["https://github.com/Mchuong10/Prog109Repo/blob/main/Artifact5/20220822_161703.jpg", "Siblings"],
-  ["https://github.com/Mchuong10/Prog109Repo/blob/main/Artifact5/20221012_181844_03.jpg","Fun at the Park" ],
-  ["https://github.com/Mchuong10/Prog109Repo/blob/main/Artifact5/20221012_205126.jpg", "Chloe"],
+  "https://github.com/Mchuong10/Prog109Repo/blob/main/Artifact5/20220618_124212.jpg",
+  "https://github.com/Mchuong10/Prog109Repo/blob/main/Artifact5/20220629_154645.jpg",
+  "https://github.com/Mchuong10/Prog109Repo/blob/main/Artifact5/20220813_122309.jpg", 
+  "https://github.com/Mchuong10/Prog109Repo/blob/main/Artifact5/20220822_161703.jpg", 
+  "https://github.com/Mchuong10/Prog109Repo/blob/main/Artifact5/20221012_181844_03.jpg", 
+  "https://github.com/Mchuong10/Prog109Repo/blob/main/Artifact5/20221012_205126.jpg", 
 ];
 
  
- var captionImages =["Laugh","Love","Lunch","Happiness","Family"];
+ var captionImages =["Dress-Up", "Ducks","Fun in the Sun","Siblings","Fun at the Park","Chloe"];
 
  var index=0; 
 
