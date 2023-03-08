dac9a65fe63b075dc699e298462dc2f18f7739b5
Mchuong10 <122072731+Mchuong10@users.noreply.github.com>
2023-03-07 16:01:33 -0800

    Update exp.js

--- Artifact5/exp.js
+++ Artifact5/exp.js
@@ -11,45 +11,47 @@ var myImages = [
 ];
 
  
- var captionImages =["Dress-Up", "Ducks","Fun in the Sun","Siblings","Fun at the Park","Chloe"];
+var captionImages = [
+  "Dress-Up",
+  "Ducks",
+  "Fun in the Sun",
+  "Siblings",
+  "Fun at the Park",
+  "Chloe",
+];
 
- var index=0; 
+var index = 0;
 
- function updateImage(){
- document.getElementById("slideshow").src = myImages[index];
- document.getElementById("slideshow").alt= captionImages[index];
- document.getElementById("caption").textContent = captionImages[index]; 
-} 
+function updateImage() {
+  document.getElementById("slideshow").src = myImages[index];
+  document.getElementById("slideshow").alt = captionImages[index];
+  document.getElementById("caption").textContent = captionImages[index];
+}
 
-function next(){
- if (myImages.length == index+1)
- index=0;
- else
- index++;
- updateImage();
-} 
- 
+function next() {
+  if (myImages.length == index + 1) index = 0;
+  else index++;
+  updateImage();
+}
 
-function back(){
- if (index===0)
- index=myImages.length-1;
- else
- index--;
- 
- updateImage();
-} 
+function back() {
+  if (index === 0) index = myImages.length - 1;
+  else index--;
 
-var nextButton = document.getElementById("next"); 
-var previousButton = document.getElementById("previous"); 
+  updateImage();
+}
 
-previousButton.addEventListener("click",back,false);
-nextButton.addEventListener("click",next,false); 
+var nextButton = document.getElementById("next");
+var previousButton = document.getElementById("previous");
+
+previousButton.addEventListener("click", back, false);
+nextButton.addEventListener("click", next, false);
 
 //autoplay
-function playmyImages(){
-	if (num==myImages.length){
-		num=0;
-		}
-	document.getElementById('slide').src=myImages[num][0];
-	num=num+1;
-	}
+function playmyImages() {
+  if (num == myImages.length) {
+    num = 0;
+  }
+  document.getElementById("slide").src = myImages[num][0];
+  num = num + 1;
+}
